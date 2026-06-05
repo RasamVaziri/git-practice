fab2144ad8311d7e60cbf25140314597233a51e8
RasamVaziri <rkhozhanets.1910@mail.ru>
2026-06-05 19:31:53 +0700

    Add check_format.sh pre-commit script

---
+++ check_format.sh
@@ -0,0 +1,13 @@
+for file in $(git diff --cached --name-only)
+do
+    case "$file" in
+        *.txt)
+            if [ ! -s "$file" ]; then
+                echo "File $file is empty, commit canceled"
+                exit 1
+            fi
+            ;;
+    esac
+done
+
+exit 0
